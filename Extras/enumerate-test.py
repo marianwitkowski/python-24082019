c79aaae78112815dca2bcc5a554a0e69e89ef970
Marian Witkowski <marian.witkowski@gmail.com>
2019-08-25 19:24:02 +0200

    extras'y

---
+++ Extras/enumerate-test.py
@@ -0,0 +1,19 @@
+"""
+Enumerate - przyklad
+"""
+
+# zadanie - wypisać zawartość listy wraz z informacją o indeksie (numeracja od 1)
+lista = ["A", "B", "C", "D"]
+
+# wersja bez Enumerate
+index = 1
+print("\nWersja bez Enumerate")
+for element in lista:
+    print(f"Element nr {index} to {element}")
+    index+=1
+
+# wersja z Enumerate
+print("\nWersja z Enumerate")
+for index, element in enumerate(lista, 1):
+    print(f"Element nr {index} to {element}")
+
