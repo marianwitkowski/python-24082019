d5050357ba7060a771c964947e233e8164fb96f0
Marian Witkowski <marian.witkowski@gmail.com>
2019-09-01 16:08:46 +0200

    Revert "work progress"
    This reverts commit f719030e

---
+++ Day02/krotki.py
@@ -0,0 +1,18 @@
+"""
+Co to sa krotki
+"""
+krotka = (1, "OK", 10.05)
+#krotka[0] = 10 - to powoduje wyjatek
+print(type(krotka))
+print(krotka)
+
+for element in krotka:
+    print(element)
+
+print("element 1 krotki to {}".format(krotka[1]))
+print("elementow w krotce jest {}".format(len(krotka)))
+print("czy NOK wystepuje w krotce {}".format("NOK" in krotka))
+
+krop_z_1_elementem = (3,10) * 3
+
+print(krop_z_1_elementem)
