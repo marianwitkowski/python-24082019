ade0dabc397c62553f1948ddd815f36bf815ca13
Marian Witkowski <marian.witkowski@gmail.com>
2019-09-02 10:51:21 +0200

    Revert "work progress"
    This reverts commit f719030e

---
+++ Extras/stos-kolejka.py
@@ -0,0 +1,37 @@
+"""
+ Kolejki i stosy
+"""
+
+from collections import deque
+
+# przykład użycia stosu
+# tworzenie
+print("**** STOS ******")
+wyrazy = deque()
+tekst = "Ala ma kota i kot jest rudy"
+for slowo in tekst.split(" "):
+    wyrazy.append(slowo)
+
+# zdejmowanie ostatniego elementow
+print(wyrazy)
+while len(wyrazy)>0:
+    ostatni = wyrazy.pop()
+    print(f"pobrany wyraz to: '{ostatni}', a pozostałe: {wyrazy}")
+
+print("="*30)
+
+
+# kolejka
+# tworzenie danych
+wyrazy = deque()
+tekst = "Ala ma kota i kot jest rudy"
+for slowo in tekst.split(" "):
+    wyrazy.append(slowo)
+
+# zdejmowanie pierwszego elementu
+print(wyrazy)
+while len(wyrazy)>0:
+    pierwszy = wyrazy.popleft()
+    print(f"pobrany wyraz z kolejki: '{pierwszy}', a pozostałe: {wyrazy}")
+
+print("="*30)
