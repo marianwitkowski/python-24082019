491d61a7d7fac4ad7c960ae49fdb9448cdba6f16
Marian Witkowski <marian.witkowski@gmail.com>
2019-09-12 13:40:33 +0200

    zadania

--- Extras/gen-los.py
+++ Extras/gen-los.py
@@ -9,7 +9,8 @@
 """
 
 import random
-
+import time
+import math
 
 class PrimeGenerator:
 
@@ -17,9 +18,24 @@ class PrimeGenerator:
     # maks wartosc dla generowanej liczby
     #
     def __init__(self, max):
-        pass
+        # inicjalizacja generatora liczb pseudolosowych
+        if max<10:
+            raise Exception("Are you serious ;) ?") # no trochę mało tych liczb pierwszych tu będzie
+        random.seed(time.time_ns()) #inicjalizacja zalążka liczb pseudolosowych
+        self._max = max
+        self._primes = list()
 
-
+    #
+    # sprawdzanie czy liczba jest pierwsza
+    #
+    def is_prime_number(self, num):
+        if (num < 2):
+            return False
+        else:
+            for i in range(2, int(math.sqrt(num)) + 1):
+                if num % i == 0:
+                    return False
+            return True
 
     #
     # generujemy liczbe z przedzialu 1, max - przy czym musi to byc liczba pierwsza
@@ -27,21 +43,39 @@ class PrimeGenerator:
     # obsłużyć rzuceniem wyjątku sytuację, gdy nie można już znaleźć "wolnej" liczby pierwszej w zakresie
     #
     def generate(self):
-        pass
+        counter = 0
+        while True:
+            counter += 1
+            if counter >= self._max:
+                raise Exception("All numbers generated")
+
+            number = random.randint(1, self._max)
+            if number in self._primes:
+                continue
+
+            if not self.is_prime_number(number):
+                continue
+
+            self._primes.append(number)
+            return number
 
 
     #
-    # "resetuje" generator - wywalamy informacje o liczbach, które były wczęsniej generowane
+    # "resetuje" generator - wywalamy informacje o liczbach, które były wcześniej generowane
     #
     def reset(self):
-        pass
+        self._primes.clear()
 
 
 ### przykład użycia ###
-primes_gen = PrimeGenerator(10000)
+primes_gen = PrimeGenerator(int(10E9))
 
-# losujemy 100 liczb pierwszych z zakresu 1-10000
-for _ in range(100):
-    print("Losowa liczba pierwsza = ",primes_gen.generate())
+# losujemy 1000 liczb pierwszych z zakresu 1-10E9
+for _ in range(1000):
+    try:
+        print("Losowa liczba pierwsza = ",primes_gen.generate())
+    except Exception as e:
+        print("Achtung!",e)
+        break
 
 primes_gen.reset()
