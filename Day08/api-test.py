f118932492f263d1b74ca78c298776fda2a61717
MW <marian.witkowski@gmail.com>
2019-09-22 11:48:42 +0200

    Day07

--- Day08/api-test.py
+++ Day08/api-test.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, make_response
+import math
 
 app = Flask("MyApiServer")
 
@@ -18,5 +19,19 @@ def papuga(name):
 @app.route('/api/ping', methods=['GET'])
 def ping():
     return create_reponse(jsonify({"status" : "OK"}), 200)
+
+@app.route('/api/isPrime/<number>')
+def check_is_prime(number):
+    try:
+        number = int(number)
+        if (number < 2):
+            return create_reponse(jsonify({"status" : "OK", "msg" : "Not prime number"}), 409)
+        else:
+            for i in range(2, int(math.sqrt(number)) + 1):
+                if number % i == 0:
+                    return create_reponse(jsonify({"status" : "OK", "msg" : "Not prime number"}), 409)
+            return create_reponse(jsonify({"status" : "OK"}), 200)
+    except Exception as exc:
+        return create_reponse(jsonify({"status" : "ERROR", "error" : str(exc) }), 500)
 
 app.run(port=1234, debug=True)
